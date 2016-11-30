8d7c5e90ea7a7cb4f390938b5d164b57cd92fd46
Josue <josuem9217@hstat.org>
2016-11-30 18:26:29 +0000

    start

---
+++ project.rb
@@ -0,0 +1,6 @@
+# https://data.cityofnewyork.us/Transportation/DOF-Parking-Violation-Codes/ncbg-6agr
+# https://data.cityofnewyork.us/api/views/ncbg-6agr/rows.json?accessType=DOWNLOAD
+# https://jsonformatter.curiousconcept.com/
+
+require 'json'
+violation_data = JSON.parse(%q||)
